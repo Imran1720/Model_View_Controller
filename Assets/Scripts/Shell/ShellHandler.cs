c15880b2acfe050a470c4a253f6b8f9091ac5e2d
Imran1720 <skimran1720@gmail.com>
2025-05-14 14:34:30 +0530

    Implemented shoot logic and particle system for it

---
+++ Assets/Scripts/Shell/ShellHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShellHandler : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem explosion;
+    private void Start()
+    {
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<TankView>() == null)
+        {
+            Instantiate(explosion, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Tank/TankAttackHandler.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TankAttackHandler : MonoBehaviour
+{
+    [SerializeField] private Rigidbody shellRb;
+    [SerializeField] private Transform shootPoint;
+    [SerializeField] private Slider aimSlider;
+
+
+    [SerializeField] private float minLaunchForce = 15f;
+    [SerializeField] private float maxLaunchForce = 30f;
+    [SerializeField] private float maxChargeTime = 0.75f;
+
+    private float currentLaunchForce;
+    private float chargeSpeed;
+
+    private bool firedShell;
+
+    private void OnEnable()
+    {
+        currentLaunchForce = minLaunchForce;
+        aimSlider.value = minLaunchForce;
+    }
+
+    private void Start()
+    {
+        chargeSpeed = (maxLaunchForce - minLaunchForce) / maxChargeTime;
+    }
+    private void Update()
+    {
+        if (currentLaunchForce >= maxLaunchForce && !firedShell)
+        {
+            currentLaunchForce = maxLaunchForce;
+            Fire();
+        }
+        else if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            firedShell = false;
+            currentLaunchForce = minLaunchForce;
+        }
+        else if (Input.GetKey(KeyCode.Mouse0) && !firedShell)
+        {
+            //charge
+            currentLaunchForce += chargeSpeed * Time.deltaTime;
+            aimSlider.value = currentLaunchForce;
+        }
+        else if (Input.GetKeyUp(KeyCode.Mouse0) && !firedShell)
+        {
+            Fire();
+        }
+
+    }
+
+    private void Fire()
+    {
+        firedShell = true;
+        aimSlider.value = minLaunchForce;
+        Rigidbody shellInstance = Instantiate(shellRb, shootPoint.position, shootPoint.rotation) as Rigidbody;
+
+        shellInstance.velocity = shootPoint.transform.forward * currentLaunchForce;
+    }
+}
